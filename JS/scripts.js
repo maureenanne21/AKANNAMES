05bcd323ff0bee406c9b755dc046eee72591859a
maureen <maureenannw@gmail.com>
2021-02-27 11:54:38 +0300

    second commit

---
+++ JS/scripts.js
@@ -0,0 +1,12 @@
+const getName = () ==> {
+    let firstName = document.getElementById("firstName").value
+let secondName = document.getElementById("secondName").value
+
+console.log(firstName, secondName) 
+
+
+
+
+
+
+} 
